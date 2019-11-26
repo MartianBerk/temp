3c0c30c062280f370693dd521abde7f27f42489a
Martin Baker <martin.baker@rimes.com>
2019-11-26 14:57:44 +0000

    tweak

--- group-apply-combine.py
+++ group-apply-combine.py
@@ -32,18 +32,41 @@ def generate_df(rows, **kwargs):
 def merge(df):
     values = [v for v in unique(df.values.ravel("K")).tolist() if v]
 
-    # return "|".join([v for v in values if v])
-    return "[\"{}\"]".format("\", \"".join(df))
+    return "[\"{}\"]".format("\", \"".join(values)) if values else "[]"
 
 
 def main():
-    df1 = generate_df(1000, ric="ric", ric2="ric2", isin="isin", isin2="isin2")
-    identifiers = {"ric": ["ric", "ric2"], "isin": ["isin", "isin2"]}
+    # df1 = generate_df(1000, ric1="ric", ric2="ric2", isin1="isin", isin2="isin2")
+    df1 = DataFrame([{"SOURCE": "foo", "SYMBOL": "bar",
+                      "RIC1": "a", "ISIN1": "b"},
+                     {"SOURCE": "foo", "SYMBOL": "bar",
+                      "RIC1": "a", "RIC2": "aa"},
+                     {"SOURCE": "foo", "SYMBOL": "bar",
+                      "ISIN1": "bb", "ISIN2": "bbb"},
+                     {"SOURCE": "foo", "SYMBOL": "baz",
+                      "RIC1": "aaa", "ISIN2": "bbb"},
+                     {"SOURCE": "foo", "SYMBOL": "baz",
+                      "RIC1": "a", "RIC2": "aa", "ISIN1": "b", "ISIN2": "bb"}],
+                    columns=["SOURCE", "SYMBOL", "RIC1", "RIC2", "ISIN1", "ISIN2"]).fillna("")
+    identifiers = {"RIC": ["RIC1", "RIC2"], "ISIN": ["ISIN1", "ISIN2"]}
+
+    bmrx_df = df1.copy(deep=False)
+    bmrx_df = bmrx_df.set_index(["SOURCE", "SYMBOL"])
 
     for i, cols in identifiers.items():
-        df1[i] = df1.groupby(["source", "symbol"])[cols].transform(merge)
-    
-    print(df1[[i for i in identifiers.keys()]])
+        df1[i] = df1[i] if i in df1.columns else ""
+        for c in cols:
+            df1[c] = df1[c] if c in df1.columns else ""
+
+        bmrx_df[i] = df1.groupby(["SOURCE", "SYMBOL"])[cols].apply(merge)
+
+    bmrx_df = bmrx_df.reset_index()
+
+    # drop duplicate rows as all identifiers have been squashed above
+    dedupe = ["SOURCE", "SYMBOL"]
+    dedupe.extend(list(identifiers.keys()))
+    bmrx_df = bmrx_df.drop_duplicates(subset=dedupe).reset_index(drop=True)
+    print(bmrx_df)
 
 
 if __name__ == "__main__":
